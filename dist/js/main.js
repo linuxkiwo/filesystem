e1e65428c1086e883a1fc03d3241f79bf3311728
Lucas Elvira <lucaselvira96@gmail.com>
2018-02-19 20:58:02 +0100

    Allow to  move one file or folder to another one folder

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,51 +1,102 @@
 'use strict';
-var EventClient = require('./../../commonModules/localEvent').Client,
-    $ = require('jquery');
+try{
+	var EventClient = require('./../../commonModules/localEvent').Client;
+} catch (e) {
+console.log(e);
+}
+var $ = require('jquery');
 
 //Preguntamos por los datos locales y SOLO los mostramos en la consola.
 /*Variables globales*/
-
+var isSelected, canMove = false, initX = 0, initY = 0;
 /*metodos globales*/
 var drawFiles
 /*modulos externos*/
 var external = {};
 
-
-
-
-
 external.drawFiles = drawFiles = (args) => {
-    /*Lista los archivos y carpetas que hay en ese direcorio*/
-    let str = '',
-        list = args[0];
-    for (var i in list['dir'])
-        str += `<li class="folder">${list['dir'][i]}</li>`;
-    for (i in list['fil'])
-        str += `<li class="file">${list['fil'][i]}</li>`;
-    $('main').html(str);
-    /*Cambia el menú de navegación */
-    //console.log(args[0][1])
-    if (args.length >=2){
-        console.log(args[1])
-        str = '<li class="track">Carpeta Personal</li>';
-        let path = args[1];
-        for (var i=2; i< path.length;i++)
-            str +=`<li class="track">${path[i]}</li>`;
-        $('.topBar').html(str);
-    }
+	/*Lista los archivos y carpetas que hay en ese direcorio*/
+	let str = args[0];
+
+	$('main ul').html(str);
+	/*Cambia el menú de navegación */
+	//console.log(args[0][1])
+	if (args.length >=2){
+		console.log(args[1])
+		str = '<li class="track">Carpeta Personal</li>';
+		let path = args[1];
+		for (var i=2; i< path.length;i++)
+			str +=`<li class="track">${path[i]}</li>`;
+		$('.topBar').html(str);
+	}
 };
 
+$('body').on('dblclick', '.folder', (e)=> {
+	/*
+	*funcion encarga de mandar el evento necesario que determina que
+	*carpeta quieren abrir
+	*/
+	let name = $(e.currentTarget).find('p').html();
+	$('.topBar').append(`<li class="track">${name}</li>`);
+	comunication.send('loadFiles', 'drawFiles', [name]);
+}).on('dblclick', '.track', (e)=>{
+	/*
+	*Funcion encarga de enviar el evento para indicar a que carpeta del camino
+	*de migas de pan generado en la topbar se quiere ir
+	*/
+	let name = $(e.currentTarget).html();
+	console.log(`name vale: ${name}`);
+	comunication.send('changeDir', 'drawFiles', [name]);
+	e.stopPropagation();
+}).on('mouseover', '.folder, .file', (e)=> {
+	/*mostrar el texto completo de la carpeta  o archivo*/
+	$(e.currentTarget).find('p').removeClass('ellipsis');
+}).on('mouseout', '.folder, .file', (e)=> {
+	/*volver a ocultar el texto completo de la carpeta  o archivo*/
+	$(e.currentTarget).find('p').addClass('ellipsis');
+}).on('click', '.folder, .file', (e)=> {
+	/*
+	*Esta función se encarga de:
+	*seleccionar un caja o archivo con un click.
+	*Cuando esta seleccionado, se puede mover dentro de .elements gracias a la
+	*función que se encuentra dos debajo.
+	*/
+	if (!canMove){
+		e.stopPropagation();
+		canMove = true;
+		isSelected = $(e.currentTarget);
+		let x = initX = e.clientX, y = initY = e.clientY;
+		isSelected.addClass('moving').css({"top": y, "left": x});
+	}
 
-
-
-$('body').on('dblclick', '.folder', function(e) {
-    let name = $(e.currentTarget).html();
-    $('.topBar').append(`<li class="track">${name}</li>`);
-    comunication.send('loadFiles', 'drawFiles', [name]);
-}).on('dblclick', '.track', function(e){
-    let name = $(e.currentTarget).html();
-    console.log(`name vale: ${name}`);
-    comunication.send('changeDir', 'drawFiles', [name]);
+}).on('click', '.folder', (e)=> {
+	/*
+	*Esta función se encarga de:
+	*Si ya hay algo seleccionado, la carpeta en la que se hace click sirve es
+	*el destino de la primera
+	*/
+	if (!isSelected) return;
+	if (isSelected.index('.elements li') === $(e.currentTarget).index('.elements li')) return;
+	e.stopPropagation();
+	let o = $(e.currentTarget).offset();
+	isSelected.animate({"top": o.top, "left": o.left},500, ()=> {isSelected.remove(); isSelected = false; canMove=false;});
+	comunication.send('move', null, [isSelected.find('p').html(), $(e.currentTarget).find('p').html()]);
+}).on('mousemove', '.elements, .elements *', (e)=> {
+	/*
+	*Funcion encarga de permitir el movimiento por el menú de los archivos o carpetas
+	*/
+	if (!canMove) return;
+	e.stopPropagation();
+	let x = e.clientX - isSelected.width(), y = e.clientY+50;
+	console.log("se mueve!")
+	isSelected.css({"top": y, "left": x});
+}).on('click', '.elements', () => {
+	/*
+	*Función que permite la deselección de un arhivo o carpeta
+	*/
+	if (!canMove) return;
+	canMove = false;
+	isSelected.animate({"top": initY, "left": initX},500, ()=>{isSelected.removeClass('moving').removeAttr("style"); isSelected = false; canMove=false;});
 });
 
 var comunication = new EventClient(external);
--- index.js
+++ index.js
@@ -1,67 +1,87 @@
 'use strict';
 /*Importación de módulos */
 
-const {app, BrowserWindow } = require('electron');
-//const { ipcMain } = require('electron');
+const { app, BrowserWindow, globalShortcut } = require('electron');
 const fs = require('fs')
 const path = require('path');
 const url = require('url');
 const { exec } = require('child_process');
 
-var EventServer = require('../commonModules/localEvent').Server;
+const EventServer = require('../commonModules/localEvent').Server;
 
 
 /* Declaración de variables globales */
 var win, currentPath, dirList, currentFiles;
 /*Declaración de los metodos que se tengan que emplear fuera*/
 /*Declaración de las funciones globales*/
-var createWin, closeWin, loadFiles, changeDir;
+var createWin, closeWin, loadFiles, changeDir, move;
 var external = {};
 
 
 exec('echo $USER', (err, stdout, stderr) => {
-    currentPath = `/home/${stdout}/`.replace('\n', '');
+	currentPath = `/home/${stdout}/`.replace('\n', '');
 });
 
 
 createWin = () => {
-    win = new BrowserWindow({ width: 800, height: 600, menu: false });
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'dist/index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
-    win.webContents.openDevTools();
-    win.on('closed', () => {
-        win = null
-    });
-
+	win = new BrowserWindow({ width: 800, height: 600, menu: false });
+	globalShortcut.register('CommandOrControl+Y', () => {
+      // Hacer algo cuando se presiona tanto Tecla Command o Control + Y.
+      console.log("algo ha pasado")
+    })
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'dist/index.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+	win.webContents.openDevTools();
+	win.on('closed', () => {
+		win = null
+	});
 };
+
 closeWin = () => app.quit();
 
 external.loadFiles = loadFiles = (dir = '') => {
-    currentPath = (dir !== '') ? (currentPath + dir[0] + '/') : currentPath;
-    currentFiles = {dir: [], fil: []};
-    var listDir = fs.readdirSync(currentPath);
-    for (let i of listDir){
-        if (i.search(/^\./) !== -1)
-            continue;
-        else if (fs.lstatSync(`${currentPath}/${i}`).isDirectory())
-            currentFiles['dir'].push(i);
-        else if (fs.lstatSync(`${currentPath}/${i}`).isFile())
-            currentFiles['fil'].push(i);
-    }
-    return [currentFiles];
+	currentPath = (dir !== '') ? (currentPath + dir[0] + '/') : currentPath;
+	currentFiles = { dir: [], fil: [] };
+	var listDir = fs.readdirSync(currentPath);
+	for (let i of listDir) {
+		if (i.search(/^\./) !== -1)
+			continue;
+		else if (fs.lstatSync(`${currentPath}/${i}`).isDirectory())
+			currentFiles['dir'].push(i);
+		else if (fs.lstatSync(`${currentPath}/${i}`).isFile())
+			currentFiles['fil'].push(i);
+	};
+	let list = currentFiles,
+		str = '';
+	for (var i in list['dir'])
+		str += `<li class="folder"><img src="media/folder.jpg"><p>${list['dir'][i]}</p></li>`;
+	for (i in list['fil'])
+		str += `<li class="file"><img src="media/file.jpg"><p>${list['fil'][i]}</p></li>`;
+	return [str];
 };
 
-external.changeDir = changeDir = (name) =>{
-        let path = currentPath.split('/');
-        currentPath = path.slice(0, path.indexOf(name[0])+1).join("/")+'/';
-        return [loadFiles()[0], path.slice(1, path.indexOf(name[0])+1)];
+external.changeDir = changeDir = (name) => {
+	let path = currentPath.split('/');
+	currentPath = path.slice(0, path.indexOf(name[0]) + 1).join("/") + '/';
+	return [loadFiles()[0], path.slice(1, path.indexOf(name[0]) + 1)];
 };
+
+external.move = move = (paths) => {
+	fs.rename(`${currentPath}${paths[0]}`, `${currentPath}${paths[1]}/${paths[0]}`, (err) => {
+		console.log(err);
+	});
+}
 
 
 var comunication = new EventServer(external);
+external.changeDir = changeDir = (name) => {
+	let path = currentPath.split('/');
+	currentPath = path.slice(0, path.indexOf(name[0]) + 1).join("/") + '/';
+	return [loadFiles()[0], path.slice(1, path.indexOf(name[0]) + 1)];
+};
 
 
 app.on('ready', createWin);
